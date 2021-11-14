0e1ff0e716eb3c3d23105945b82dad73e2b01fb2
eltonpignatel <elton.pignatel@gmail.com>
2021-11-14 14:50:11 -0300

    Início de projeto rodando com spring boot e oracle

---
+++ src/main/java/br/com/eltonpignatel/app/Db1Pdi2Application.java
@@ -0,0 +1,11 @@
+package br.com.eltonpignatel.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db1Pdi2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Db1Pdi2Application.class, args);
+	}
+}
